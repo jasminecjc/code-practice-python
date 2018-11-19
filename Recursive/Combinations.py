995db75cb2ac8d14a4e75beab35d3f767ad71849
Cheng Jia Chen <chengjc@fenbi.com>
2018-11-19 18:43:35 +0800

    rm

--- Recursive/Combinations.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
-Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
-'''
--- Recursive/Permutations.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
-Given a collection of distinct integers, return all possible permutations.
-'''
-class Solution:
- def permutations(self, nums):
-   pass
