1a19a5bfe9baf0e19dd99eef057a1b03cbe41c53
Cheng Jia Chen <chengjc@fenbi.com>
2018-12-06 19:35:50 +0800

    update

--- TwoPointer/3Sum.py
+++ TwoPointer/3Sum.py
@@ -6,3 +6,28 @@ The solution set must not contain duplicate triplets.
 '''
 class Solution(object):
   def threeSum(self, nums):
+    nums = sorted(nums)
+    res = []
+    for idx, val in enumerate(nums):
+      if idx and val == nums[i - 1]:
+        continue
+      l, r = idx + 1, len(nums) - 1
+      while l < r:
+        ans = nums[l] + nums[r]
+        if ans == -val:
+          res.append([val, nums[l], nums[r]])
+          while l < r and nums[l] == nums[l - 1]:
+            l += 1
+          while l < r and nums[r] == nums[r - 1]:
+            r -= 1
+          l += 1
+          r -= 1
+        elif ans < -val:
+          l += 1
+        else:
+          r -= 1 
+    return res
+
+test = Solution()
+arr = [-1,0,1,2,-3,-1,-1,3]
+print(test.threeSum(arr))
